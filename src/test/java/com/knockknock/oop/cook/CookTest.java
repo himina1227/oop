ac112ee04105a0e0eeea8f98fac4d833c8be83b3
himina1227 <himina1227@gmail.com>
2024-09-21 00:17:51 +0900

    fix: Cook Exception 테스트 코드 추가

--- src/test/java/com/knockknock/oop/cook/CookTest.java
+++ src/test/java/com/knockknock/oop/cook/CookTest.java
@@ -4,12 +4,20 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CookTest {
 
     @DisplayName("요리를 생성한다.")
     @Test
-    void create_success() {
-        Assertions.assertThatCode(() -> new Cook("닭갈비", 10000))
+    void create_Success() {
+        assertThatCode(() -> new Cook("닭갈비", 10000))
                 .doesNotThrowAnyException();
     }
+
+//    @Test
+//    void create_WhenPriceIsZero_ThrowsException() {
+//        assertThatCode(() -> new Cook("닭갈비", 0))
+//                .isInstanceOf(NotPriceZeroException.class);
+//    }
 }
