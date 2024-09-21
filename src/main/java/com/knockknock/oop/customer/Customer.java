e697d79da354ce4b6ef5c092d671d32a1063f0f4
himina1227 <himina1227@gmail.com>
2024-09-21 12:25:16 +0900

    fix: Customer 클래스 order메서드 추가

--- src/main/java/com/knockknock/oop/customer/Customer.java
+++ src/main/java/com/knockknock/oop/customer/Customer.java
@@ -1,4 +1,14 @@
 package com.knockknock.oop.customer;
 
+import com.knockknock.oop.cook.Cook;
+import com.knockknock.oop.cooking.Cooking;
+import com.knockknock.oop.menu.Menu;
+import com.knockknock.oop.menu.item.MenuItem;
+
 public class Customer {
+    public static void order(String menuName, Menu menu, Cooking cooking) {
+        MenuItem menuItem = menu.choose(menuName);
+        Cook cook = cooking.makeCook(menuItem);
+
+    }
 }
--- src/test/java/com/knockknock/oop/customer/CustomerTest.java
+++ src/test/java/com/knockknock/oop/customer/CustomerTest.java
@@ -1,11 +1,22 @@
 package com.knockknock.oop.customer;
 
+import com.knockknock.oop.cooking.Cooking;
+import com.knockknock.oop.menu.Menu;
+import com.knockknock.oop.menu.item.MenuItem;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class CustomerTest {
 
     @Test
     void orderTest() {
         Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 10000), new MenuItem("냉면", 10000)));
+        Cooking cooking = new Cooking();
+        assertThatCode(() -> Customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
